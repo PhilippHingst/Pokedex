3334966472a9d954e73508f48bb9627702b9c490
PhilippHingst <hingst.philipp@web.de>
2023-07-27 07:21:28 +0200

    Searchbar and Carddesign

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ async function loadPokemon() {
   loadSpeciesInfo();
 }
 
+async function loadPokemonNames() {
+  let url = 'https://pokeapi.co/api/v2/pokemon?limit=1010';
+  let response = await fetch(url);
+  let data = await response.json();
+  let pokemonIds = data.results.map(pokemon => pokemon.url.split('/')[6]);
+  
+  let datalist = document.getElementById('pokemonNames');
+  datalist.innerHTML = '';
+
+  pokemonIds.forEach(id => {
+    loadPokemonNameInLanguage(id, currentLanguage);
+  });
+}
+
+async function loadPokemonNameInLanguage(id, language) {
+  let url = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
+  let response = await fetch(url);
+  let data = await response.json();
+
+  let nameInDesiredLanguage = data.names.find(name => name.language.name === language).name;
+
+  let datalist = document.getElementById('pokemonNames');
+  let option = document.createElement('option');
+  option.value = nameInDesiredLanguage;
+  datalist.appendChild(option);
+}
+
 async function loadSpeciesInfo() {
   let url = `https://pokeapi.co/api/v2/pokemon-species/${currentOrder}/`;
   let responseStat = await fetch(url);
@@ -30,14 +57,14 @@ async function loadElementInfo() {
 }
 
 function renderPokemonInfo() {
-  document.getElementById('element').innerHTML = currentElementInfo['names']['6']['name'];
+  let elementNameInGerman = currentElementInfo['names'].find(name => name.language.name === 'de');
+  document.getElementById('element').innerHTML = elementNameInGerman ? elementNameInGerman.name : '';
   document.getElementById('order').innerHTML = currentStatInfo['id'];
   document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
   let pokemonFrontSprite = currentPokemon['sprites']['other']['official-artwork']['front_default'];
   document.getElementById('pokemonFrontSprite').src = pokemonFrontSprite;
   let currentColor = currentStatInfo['color']['name'];
   document.getElementById('pokedex').classList.add(currentColor);
-
 }
 
 function renderPokeStats() {
@@ -52,3 +79,46 @@ function renderPokeStats() {
   statContent.innerHTML = ``;
   statContent.innerHTML += generateStatsContent();
 }
+
+let currentLanguage = 'de'; // Set the default language to German
+
+function changeLanguage() {
+  let languageSelect = document.getElementById('languageSelect');
+  currentLanguage = languageSelect.value;
+  loadPokemonNames();
+}
+
+async function searchPokemon() {
+  let input = document.getElementById('search-input');
+  let inputValue = input.value.toLowerCase();
+  
+  // Verify if input is a number or a name
+  if (isNaN(inputValue)) {
+    // Find the Pokemon by name
+    let url = `https://pokeapi.co/api/v2/pokemon/${inputValue}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (data) {
+      currentPokemon = data;
+      currentOrder = data.id;
+      loadSpeciesInfo();
+    } else {
+      alert("Pokemon not found");
+    }
+  } else {
+    // Find the Pokemon by number
+    let url = `https://pokeapi.co/api/v2/pokemon/${inputValue}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if (data) {
+      currentPokemon = data;
+      currentOrder = data.id;
+      loadSpeciesInfo();
+    } else {
+      alert("Pokemon not found");
+    }
+  }
+}
+
